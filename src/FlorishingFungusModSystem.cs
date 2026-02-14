5013800fe7f2425fea0af39905a5eebefe7dd8f4
Loafcifer <taing897@gmail.com>
2026-02-14 14:19:01 +1100

    Update Mod Info and Mod System
    Changed Mod into to next version and removed code in mod system files that wasn't needed.

--- src/FlorishingFungusModSystem.cs
+++ src/FlorishingFungusModSystem.cs
@@ -8,24 +8,9 @@ namespace FlorishingFungus.src
 {
     public class FlorishingFungusModSystem : ModSystem
     {
-
-        // Called on server and client
-        // Useful for registering block/entity classes on both sides
         public override void Start(ICoreAPI api)
         {
-            Mod.Logger.Notification("Hello from template mod: " + api.Side);
-
             api.RegisterEntityBehaviorClass(Mod.Info.ModID + ".florishfungus", typeof(BehaviorFlorishFungus));
-        }
-
-        public override void StartServerSide(ICoreServerAPI api)
-        {
-            Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("florishingfungus:hello"));
-        }
-
-        public override void StartClientSide(ICoreClientAPI api)
-        {
-            Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("florishingfungus:hello"));
         }
 
     }
